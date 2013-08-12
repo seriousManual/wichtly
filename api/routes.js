225b11a3434fb6ae31d25cf6f7c18af3948fbceb
zaphod1984 <zaphod84@gmx.de>
2013-08-12 18:28:12 +0200

    little update

---
+++ api/routes.js
@@ -0,0 +1,21 @@
+function install(app) {
+
+}
+
+module.exports.install = install;
+
+// GET /api/wish
+// GET /api/wish/:id
+// POST /api/wish/:id
+// PUT /api/wish
+// DELETE /api/wish
+
+/*
+        {
+            id: 1,
+            ownerId: 2,
+            headLine: '',
+            text: '',
+            bought: false 
+        }
+*/
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 
+var routes = require('./api/routes');
+
 var app = express();
+var port = 8000;
 
 app.use(express.static(__dirname + '/app'));
 
-app.listen(8000);
+routes.install(app);
+
+app.listen(port);
+
+console.log( 'listening to: ' + port );
