1ba963d0415b9a80d501935aa19abce413a9f76d
zaphod1984 <zaphod84@gmx.de>
2013-08-15 14:05:46 +0200

    added command line param

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ var path = require('path');
 
 var express = require('express');
 var browserify = require('connect-browserify');
+var argv = require('optimist').argv;
 
 var routes = require('./api/routes');
 
 var app = express();
-var port = 8000;
+var port = argv.port || 8000;
 
 app.use('/js/app.js', browserify.serve({ entry: path.join(__dirname, 'app/js/app.js') }));
 app.use(express.static(__dirname + '/app'));
