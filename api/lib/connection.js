d8c7f5bd97b7656fea8c4ab017eaee52e95ba2f1
zaphod1984 <zaphod84@gmx.de>
2013-12-05 21:40:33 +0100

    added debug

--- api/lib/connection.js
+++ api/lib/connection.js
@@ -4,6 +4,8 @@ var mongoose = require('mongoose');
 
 var configuration = require('./configuration');
 
+var d = require('debug')('wichtly:connection');
+
 module.exports = function(callback) {
 
     var uri;
@@ -13,6 +15,7 @@ module.exports = function(callback) {
         uri = util.format('mongodb://%s:%s@%s:%d/%s', configuration.database.user, configuration.database.password, configuration.database.host, configuration.database.port, configuration.database.database);
     }
 
+    d('using uri %s', uri);
     mongoose.connect(uri);
 
     var connection = mongoose.connection;
