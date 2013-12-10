552417a1136fdefd33397c0ae580a19488143b1c
zaphod1984 <zaphod84@gmx.de>
2013-12-10 21:10:52 +0100

    added enterDirective

---
+++ app/src/directives/enterDirective.js
@@ -0,0 +1,17 @@
+function enterDirective() {
+    return function(scope, element, attrs) {
+        element.bind("keydown keypress", function(event) {
+            if(event.which === 13) {
+                scope.$apply(function(){
+                    scope.$eval(attrs.ngEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+}
+
+module.exports.install = function(app) {
+    app.directive('ngEnter', enterDirective);
+};
